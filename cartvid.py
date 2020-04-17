7b13a2d9e4c498179731c492053b1b77b9b19919
mugiwala <43480121+mugiwala@users.noreply.github.com>
2020-04-17 20:55:09 +0530

    Update cartvid.py

--- cartvid.py
+++ cartvid.py
@@ -1,33 +1,33 @@
 import cv2
 
-num_down = 2       
-num_bilateral = 7  
+ndown = 2      
+nbilateral = 7  
 
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
 
-    _, img_rgb = cap.read()
+    _, irgb = cap.read()
 
-    img_color = img_rgb
-    for _ in range(num_down):
-        img_color = cv2.pyrDown(img_color)
-    
-    for _ in range(num_bilateral):
-        img_color = cv2.bilateralFilter(img_color, d=9, sigmaColor=9, sigmaSpace=7)
+    icolor = irgb
+    for _ in range(ndown):
+        icolor = cv2.pyrDown(icolor)
+        
+    for _ in range(nbilateral):
+        icolor = cv2.bilateralFilter(icolor, d=9, sigmaColor=9, sigmaSpace=7)
 
-    for _ in range(num_down):
-        img_color = cv2.pyrUp(img_color)
+    for _ in range(ndown):
+        icolor = cv2.pyrUp(icolor)
 
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-    img_blur = cv2.medianBlur(img_gray, 7)
+    gray = cv2.cvtColor(irgb, cv2.COLOR_RGB2GRAY)
+    blur = cv2.medianBlur(gray, 7)
 
-    img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=9,C=2)
+    iedge = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=9,C=2)
 
-    img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-    img_cartoon = cv2.bitwise_and(img_color, img_edge)
+    iedge = cv2.cvtColor(iedge, cv2.COLOR_GRAY2RGB)
+    icartoon = cv2.bitwise_and(icolor, iedge)
 
-    cv2.imshow("cartoon", img_cartoon)
+    cv2.imshow("cartoon", icartoon)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
