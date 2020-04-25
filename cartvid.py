4525da9c5d28193642407948fc1545693c6a1045
mugiwala <43480121+mugiwala@users.noreply.github.com>
2020-04-26 01:17:50 +0530

    Update cartvid.py

--- cartvid.py
+++ cartvid.py
@@ -1,18 +1,20 @@
 import cv2
 
-ndown = 2      
-nbilateral = 7  
+ndown = 2       
+nbilateral = 70
 
 cap = cv2.VideoCapture(0)
 
+i = 1
+
 while cap.isOpened():
 
-    _, irgb = cap.read()
+    _,  irgb = cap.read()
 
     icolor = irgb
     for _ in range(ndown):
         icolor = cv2.pyrDown(icolor)
-        
+
     for _ in range(nbilateral):
         icolor = cv2.bilateralFilter(icolor, d=9, sigmaColor=9, sigmaSpace=7)
 
@@ -22,13 +24,17 @@ while cap.isOpened():
     gray = cv2.cvtColor(irgb, cv2.COLOR_RGB2GRAY)
     blur = cv2.medianBlur(gray, 7)
 
-    iedge = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=9,C=2)
+    iedge = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=5,C=2)
 
     iedge = cv2.cvtColor(iedge, cv2.COLOR_GRAY2RGB)
     icartoon = cv2.bitwise_and(icolor, iedge)
 
     cv2.imshow("cartoon", icartoon)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    if cv2.waitKey(1) == ord('s'):
+        cv2.imwrite('anim_'+str(i)+'.jpg',icartoon)
+        i+=1
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
